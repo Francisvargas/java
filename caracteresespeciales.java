d49d54be0a0904071fc809ab6ae1500891a1585d
Francis <202213861a@utea.edu.pe>
2024-01-15 11:24:28 -0500

    segunda commit

---
+++ caracteresespeciales.java
@@ -0,0 +1,8 @@
+public class caracteresespeciales {
+  public static void main(String[] args){
+
+    System.out.print("cuando programa 💪estoy");
+    System.out.print ("muy ☺️ y me siento como un 🤴 .");
+  }
+
+}
---
+++ salidaformateada.java
@@ -0,0 +1,8 @@
+public class salidaformateada {
+    public static void main(String[] args) {
+    System.out.printf(" El numero %d notiene decimales. \n",21);
+    System.out.printf("El numero %f sale con las decimales. \n",  21.0);
+    System.out.printf("El %.3f sale exactamente con 3 decimales. \n", 21.0);
+    
+    }
+ }
