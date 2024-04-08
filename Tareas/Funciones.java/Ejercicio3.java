1eaca028766a04014f98845ee654652130b2e318
Francis <202213861a@utea.edu.pe>
2024-04-08 10:46:28 -0500

    semana 8 funciones

---
+++ Tareas/Funciones.java/Ejercicio3.java
@@ -0,0 +1,14 @@
+package Tareas.Funciones.java;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 9999; i++) {
+            if (varias.esCapicua(i)) {
+              System.out.print(i + "  ");
+            }
+          }
+        }
+      
+    }
+    
+
